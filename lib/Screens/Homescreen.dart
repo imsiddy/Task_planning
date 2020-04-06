5f2b44becb16668496c41e509f6a461a0e36558e
imsiddy <42533510+imsiddy@users.noreply.github.com>
2020-04-07 00:10:25 +0530

    Update Homescreen.dart

--- lib/Screens/Homescreen.dart
+++ lib/Screens/Homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScr extends StatefulWidget {
   HomeScr({Key key}) : super(key: key);
@@ -13,8 +14,23 @@ class _HomeScrState extends State<HomeScr> {
     return SafeArea(
       child: Scaffold(
         body: Container(
-          child: Align(
-              alignment: Alignment.center, child: Text("Data must be here")),
+          child: Container(
+            height: 300.0,
+            color: Colors.transparent,
+            child: new Container(
+                decoration: new BoxDecoration(
+                    color : const Color(0xFFF9BE7C),
+                    borderRadius: new BorderRadius.only(
+                      bottomLeft: const Radius.circular(40.0),
+                      bottomRight: const Radius.circular(40.0),
+                    )),
+                child: new Center(
+                  child: new Text("Hi modal sheet"),
+                )),
+
+            // child: Align(
+            //     alignment: Alignment.center, child: Text("Data must be here")),
+          ),
         ),
       ),
     );
