39cf0b8fb5f7998374c07bc53b22f317779f9911
imsiddy <42533510+imsiddy@users.noreply.github.com>
2020-04-21 21:39:41 +0530

    Update Tasks.dart

--- lib/Screens/Tasks.dart
+++ lib/Screens/Tasks.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:task_planner/Screens/Addtask.dart';
 
 class Tasklist extends StatefulWidget {
   Tasklist({Key key}) : super(key: key);
@@ -14,9 +15,14 @@ class _TasklistState extends State<Tasklist> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(
-          Icons.arrow_back_ios,
-          color: Colors.black,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
         backgroundColor: Color(0xFFFFF9EC),
         elevation: 0,
@@ -24,8 +30,47 @@ class _TasklistState extends State<Tasklist> {
       body: ListView(
         children: <Widget>[
           Container(
+            height: MediaQuery.of(context).size.height,
             color: Color(0xFFFFF9EC),
-            child: Container(),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: ScreenUtil().setWidth(60),
+                      vertical: ScreenUtil().setHeight(80)),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "Today",
+                        style: TextStyle(
+                            fontSize: ScreenUtil().setSp(120),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Flexible(fit: FlexFit.tight, child: SizedBox()),
+                      RaisedButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(100.0),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: ScreenUtil().setHeight(20),horizontal: ScreenUtil().setWidth(20)),
+                          child: Text("Add Task",style:TextStyle(fontSize:ScreenUtil().setSp(60) ),),
+                        ),
+                        color: Color(0xFF309397),
+                        textColor: Colors.white,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Addtsk(),
+                            ),
+                          );
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
