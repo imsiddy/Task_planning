42c2a24ecce6f3cc2ac8f2183cacc4c39d5c36fe
imsiddy <42533510+imsiddy@users.noreply.github.com>
2020-05-02 23:47:01 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:task_planner/Screens/Cleancla.dart';
-// import 'package:task_planner/Screens/calender.dart';
+import 'package:task_planner/Screens/calexample.dart';
 import 'Screens/Homedemo.dart';
-// import 'Screens/Homescreen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -16,11 +13,12 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: (context, child) =>
-          MediaQuery(data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true), child: child),
+      builder: (context, child) => MediaQuery(
+          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+          child: child),
       debugShowCheckedModeBanner: false,
-      //  home: CalendarScreen(),
        home: HomeScr(),
+      // home: Container(child: MyHomePage()),
     );
   }
 }
