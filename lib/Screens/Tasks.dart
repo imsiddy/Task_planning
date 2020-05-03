7671422c3a6cd0e32ecafb4bf6782b25124a79ed
imsiddy <42533510+imsiddy@users.noreply.github.com>
2020-05-03 23:54:20 +0530

    Update Tasks.dart

--- lib/Screens/Tasks.dart
+++ lib/Screens/Tasks.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:task_planner/Screens/calexample.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:task_planner/Screens/Addtask.dart';
 import 'package:calendar_strip/calendar_strip.dart';
@@ -10,6 +11,74 @@ class Tasklist extends StatefulWidget {
   @override
   _TasklistState createState() => _TasklistState();
 }
+
+// class _TasklistState extends State<Tasklist> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         leading: IconButton(
+//           icon: Icon(
+//             Icons.arrow_back_ios,
+//             color: Colors.black,
+//           ),
+//           onPressed: () {
+//             Navigator.pop(context);
+//           },
+//         ),
+//         backgroundColor: Color(0xFFFFF9EC),
+//         elevation: 0,
+//       ),
+//       body: ListView(
+//         children: <Widget>[
+//           Container(
+//             height: MediaQuery.of(context).size.height,
+//             color: Color(0xFFFFF9EC),
+//             child: Column(
+//               children: <Widget>[
+//                 Padding(
+//                   padding: EdgeInsets.symmetric(
+//                       horizontal: ScreenUtil().setWidth(60),
+//                       vertical: ScreenUtil().setHeight(80)),
+//                   child: Row(
+//                     children: <Widget>[
+//                       Text(
+//                         "Today",
+//                         style: TextStyle(
+//                             fontSize: ScreenUtil().setSp(100),
+//                             fontWeight: FontWeight.bold),
+//                       ),
+//                       Flexible(fit: FlexFit.tight, child: SizedBox()),
+//                       RaisedButton(
+//                         shape: RoundedRectangleBorder(
+//                           borderRadius: new BorderRadius.circular(100.0),
+//                         ),
+//                         child: Padding(
+//                           padding: EdgeInsets.symmetric(vertical: ScreenUtil().setHeight(20),horizontal: ScreenUtil().setWidth(20)),
+//                           child: Text("Add Task",style:TextStyle(fontSize:ScreenUtil().setSp(60) ),),
+//                         ),
+//                         color: Color(0xFF309397),
+//                         textColor: Colors.white,
+//                         onPressed: () {
+//                           Navigator.push(
+//                             context,
+//                             MaterialPageRoute(
+//                               builder: (context) => Addtsk(),
+//                             ),
+//                           );
+//                         },
+//                       )
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
 class _TasklistState extends State<Tasklist> {
   @override
@@ -35,39 +104,55 @@ class _TasklistState extends State<Tasklist> {
             color: Color(0xFFFFF9EC),
             child: Column(
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: ScreenUtil().setWidth(60),
-                      vertical: ScreenUtil().setHeight(80)),
-                  child: Row(
-                    children: <Widget>[
-                      Text(
-                        "Today",
-                        style: TextStyle(
-                            fontSize: ScreenUtil().setSp(120),
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Flexible(fit: FlexFit.tight, child: SizedBox()),
-                      RaisedButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(100.0),
+                Flexible(
+                  flex: 2,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: ScreenUtil().setWidth(60),
+                        vertical: ScreenUtil().setHeight(80)),
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          "Today",
+                          style: TextStyle(
+                              fontSize: ScreenUtil().setSp(100),
+                              fontWeight: FontWeight.bold),
                         ),
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(vertical: ScreenUtil().setHeight(20),horizontal: ScreenUtil().setWidth(20)),
-                          child: Text("Add Task",style:TextStyle(fontSize:ScreenUtil().setSp(60) ),),
-                        ),
-                        color: Color(0xFF309397),
-                        textColor: Colors.white,
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Addtsk(),
+                        Flexible(fit: FlexFit.tight, child: SizedBox()),
+                        RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(100.0),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: ScreenUtil().setHeight(20),
+                                horizontal: ScreenUtil().setWidth(20)),
+                            child: Text(
+                              "Add Task",
+                              style:
+                                  TextStyle(fontSize: ScreenUtil().setSp(60)),
                             ),
-                          );
-                        },
-                      )
-                    ],
+                          ),
+                          color: Color(0xFF309397),
+                          textColor: Colors.white,
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => Addtsk(),
+                              ),
+                            );
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Flexible(
+                  flex: 5,
+                  fit: FlexFit.loose,
+                  child: Container(
+                    child: MyHomePage(),
                   ),
                 ),
               ],
